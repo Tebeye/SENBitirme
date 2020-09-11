74c71a742d121d0313a71dfee9a2f6a96ed6bba3
ozavat <ozan.ozgun@bahcesehir.edu.tr>
2020-09-11 18:16:23 +0300

    User login connected to database.

--- BitirmeProjesiArayuzProjesi/BitirmeProjesiArayuzProjesi/LoginPage.cs
+++ BitirmeProjesiArayuzProjesi/BitirmeProjesiArayuzProjesi/LoginPage.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace BitirmeProjesiArayuzProjesi
 {
     public partial class LoginPage : Form
     {
-        string MyConnection2 = "Server=127.0.0.1;Database=new_schema;Uid=root;Pwd=BlVH5thGSFfHE209Nt4E;";
+        
 
         public LoginPage()
         {
@@ -32,10 +33,13 @@ namespace BitirmeProjesiArayuzProjesi
         }
         public class UserNamePassword
         {
-            public void checkUserNamePassword(object username, object password)
+            public void checkUserNamePassword(object username, object password ,string usernameDB ,string passDB)
             {
+                
 
-                if (username.ToString() == "username" && password.ToString() == "password")
+ 
+
+                if (username.ToString() == usernameDB && password.ToString() == passDB)
                 {
                     userLogged = true;
                 }
@@ -49,10 +53,30 @@ namespace BitirmeProjesiArayuzProjesi
         private void btn_login_Click(object sender, EventArgs e)
         {
 
+
+            string MyConnection2 = "Server=127.0.0.1;Database=new_schema;Uid=root;Pwd=BlVH5thGSFfHE209Nt4E;";
+            string query = "SELECT * FROM users where username=@username";
+            MySqlConnection connection = new MySqlConnection(MyConnection2);
+            MySqlCommand command = new MySqlCommand(query, connection);
+            MySqlDataAdapter myAdapter = new MySqlDataAdapter();
+            command.Parameters.AddWithValue("@username", textBox4.Text);
+            myAdapter.SelectCommand = command;
+            DataTable table = new DataTable();
+            myAdapter.Fill(table);
+
+            // Bütün tabloyu alıp aynı mysql "where" kısmı gibi içinden veri aratabilirsin
+            DataRow[] row = table.Select();
+            // altta row[0] var. Burası sürekli böyle kalıcak içinden username veya başka bir sütun çekebilirsin
+             
+
+
+            string password = row[0]["password"].ToString();
+            string username = row[0]["username"].ToString();
+
             try
             {
                 UserNamePassword user = new UserNamePassword();
-                user.checkUserNamePassword(textBox4.Text, textBox3.Text);
+                user.checkUserNamePassword(textBox4.Text, textBox3.Text,username, password);
             }
             catch (UserIsNotValidException ex)
             {
